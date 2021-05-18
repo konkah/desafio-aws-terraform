5db525b215bf79e8d5ced82813f48ee77ed7e3b2
Karlos <karlos.braga@inmetrics.com.br>
2021-05-18 01:53:41 -0300

    Created RDS

---
+++ Terraforms/rds_mysql_produtos.tf
@@ -0,0 +1,19 @@
+resource "aws_db_instance" "desafio_AWS_produtos" {
+    allocated_storage = 10
+    engine = "mysql"
+    instance_class = "db.t2.micro"
+    name = "aws_rds_produtos"
+    username = "user"
+    password = "PassworD"
+    skip_final_snapshot = true
+    db_subnet_group_name = aws_db_subnet_group.desafio_AWS_produtos.name
+
+    tags = var.tags
+}
+
+resource "aws_db_subnet_group" "desafio_AWS_produtos" {
+    name = "produtos_group"
+    subnet_ids = [ aws_subnet.desafio_AWS_produtos_db.id ]
+
+    tags = var.tags
+}
