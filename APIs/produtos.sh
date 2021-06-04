73ddf9251606463154b69278d772892979c0ab8e
Karlos <karlos.braga@inmetrics.com.br>
2021-06-04 01:31:03 -0300

    added api scripts

---
+++ APIs/produtos.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+sudo apt update
+sudo apt upgrade -y
+sudo apt autoremove -y
+sudo apt autoclean -y
+
+sudo mkdir /var/www
+sudo chmod 777 /var/www
+curl -s -L -o /var/www/project.zip https://github.com/konkah/desafio-api-produtos/archive/refs/heads/main.zip
+cd /var/www/
+
+sudo apt install -y unzip
+unzip project.zip
+mv desafio-api-produtos-main/api_produtos/* /var/www
+rm -r desafio-api-produtos-main
+rm project.zip
+
+sudo apt install -y python3.8
+sudo apt install python3-pip -y
+sudo apt install python3-dev -y 
+sudo apt install libmysqlclient-dev -y
+
+python3 -m pip install -U pip
+python3 -m pip install -U setuptools
+python3 -m pip install -U wheel
+python3 -m pip install -r requirements.txt
+
+nohup python3 manage.py runserver 0.0.0.0:80
