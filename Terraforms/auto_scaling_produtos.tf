92cd1f946920680420eaa1ffdc29695aab72d231
Karlos <karlos.braga@inmetrics.com.br>
2021-06-04 16:54:09 -0300

    allow both bastion to access api 22

--- Terraforms/auto_scaling_produtos.tf
+++ Terraforms/auto_scaling_produtos.tf
@@ -62,11 +62,11 @@ resource "aws_security_group" "desafio_AWS_produtos" {
   }
 
   ingress {
-    description = "SSH"
+    description = "Bastion"
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["10.0.0.0/24"]
+    cidr_blocks = ["10.0.0.0/23"]
   }
 
   egress {
