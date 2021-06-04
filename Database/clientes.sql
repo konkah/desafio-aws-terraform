6ff4043f66677ef23228f916b0cc4d76f8ebf1c2
Karlos <karlos.braga@inmetrics.com.br>
2021-06-04 01:27:50 -0300

    added table columns

--- Database/clientes.sql
+++ Database/clientes.sql
@@ -1,9 +1,27 @@
 use aws_rds_clientes;
 
-create table tbl_clientes (
-    ID int auto_increment primary key
+create table api_tbl_clientes (
+    ID int auto_increment primary key,
+    nome_completo_cliente varchar(200),
+    cpf varchar(15),
+    email varchar(200),
+    data_nascimento date,
+    numero_telefone varchar(15),
+    deletado tinyint
 );
 
-create table tbl_enderecos (
-    ID int auto_increment primary key
+create table api_tbl_enderecos (
+    ID int auto_increment primary key,
+    rua varchar(200),
+    numero varchar(10),
+    complemento varchar(50),
+    cep varchar(9),
+    cidade varchar(200),
+    estado varchar(200),
+    pais varchar(200),
+    cliente_id int,
+    deletado tinyint,
+    constraint fk_enderecos_clientes
+        foreign key (cliente_id)
+        references api_tbl_clientes (ID)
 );
--- Database/produtos.sql
+++ Database/produtos.sql
@@ -1,9 +1,16 @@
 use aws_rds_produtos;
 
 create table tbl_produtos (
-    ID int auto_increment primary key
+    ID int auto_increment primary key,
+    nome_produto varchar(200),
+    preco_produto double,
+    disponivel_produto tinyint,
+    deletado tinyint
 );
 
 create table tbl_inventarios (
-    ID int auto_increment primary key
+    ID int auto_increment primary key,
+    cliente_id int,
+    produto_id int,
+    deletado tinyint
 );
