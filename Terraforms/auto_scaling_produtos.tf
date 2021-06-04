4c895fa22f283326742c571f555303d50d0622dc
Karlos <karlos.braga@inmetrics.com.br>
2021-06-04 15:05:23 -0300

    allowed all subnets to access apis

--- Terraforms/auto_scaling_produtos.tf
+++ Terraforms/auto_scaling_produtos.tf
@@ -58,7 +58,7 @@ resource "aws_security_group" "desafio_AWS_produtos" {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = ["10.0.0.0/24"]
+    cidr_blocks = ["10.0.0.0/16"]
   }
 
   ingress {
